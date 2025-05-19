0be408268c71f2b5eda4cb38e2565b3c66267d5f
Ademir José Ferreira Júnior <ademirj.ferreirajunior@gmail.com>
2025-05-18 23:05:15 -0300

    test: mock CSS imports in jest

--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -20,6 +20,7 @@ const config = {
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/src/__tests__/assetMock.ts",
     ...pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
